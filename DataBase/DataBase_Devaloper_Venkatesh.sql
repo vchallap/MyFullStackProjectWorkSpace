37af92da9a5d946efb26117f291b3268371d92d6
vchallap <venkateshchallapalli138@gmail.com>
2023-03-03 01:18:16 -0500

    database quries were added

---
+++ DataBase/DataBase_Devaloper_Venkatesh.sql
@@ -0,0 +1,25 @@
+select * from venkat_customers
+--how to create table:
+create table demo1(
+
+demo_id number,
+demo_name varchar(50),
+demo_gender varchar(20),
+demo_location varchar(30));
+--retrive data from table
+SELECT * FROM demo1
+
+--Insert data into table
+--option1: by interting data with required columns
+insert into demo1 (demo_id,demo_name,demo_gender,demo_location) values(101,'abc','male','hyd');
+--option 2: we can insert all the data of tables without columns
+insert into demo1 values(102,'def','female','vojayawada');
+--option 3:we can insert data into table continuosly
+insert into demo1 values (&demo_id,&demo_name,&demo_gender,&demo_location);
+
+--commit the data to save the data perminently
+commit;
+
+--how to update to all rows in the table
+
+update table demo1
